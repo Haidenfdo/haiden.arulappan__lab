8d2f48a71961a9863710ed6c0b87db2aab380b25
Haiden Arulappan <you@example.com>
2022-03-30 17:09:25 +0530

    add

---
+++ java/math.java
@@ -0,0 +1,19 @@
+public class math {
+    public static void main(String[] args) {
+
+        // finding maximam values
+
+        int maximum = Math.max(25, 90);
+        System.out.println(maximum);
+
+        // finding square root
+
+        System.out.println(Math.sqrt(64));
+
+        // getting random number between 0 to 100
+
+        int randomnumber = (int) (Math.random() * 101);
+        System.out.println(randomnumber);
+
+    }
+}
---
+++ java/strings.java
@@ -0,0 +1,18 @@
+public class strings {
+
+    public static void main(String[] args) {
+
+        String text = "haiden is the smartest person in the world";
+        System.out.println(text);
+
+        // finding index
+
+        System.out.println(text.indexOf("person"));
+
+        // Special characters
+
+        String sentence = "The best \"software engineers\" in future are FWSA batch two";
+        System.out.println(sentence);
+
+    }
+}

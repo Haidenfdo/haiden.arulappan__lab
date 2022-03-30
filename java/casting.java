5e6905ed6a6a7172b9f7bea1cbf0bbc4dde1b431
Haiden Arulappan <you@example.com>
2022-03-30 15:03:33 +0530

    add

---
+++ java/casting.java
@@ -0,0 +1,20 @@
+public class casting {
+    public static void main(String[] args) {
+
+        // Widening casting
+        int mynum = 8;
+        double mydouble = mynum;
+
+        System.out.println(mynum);
+        System.out.println(mydouble);
+
+        // Narrow casting
+
+        double mydoub = 9.75;
+        int myint = (int) mydoub;
+
+        System.out.println(mydoub);
+        System.out.println(myint);
+
+    }
+}
---
+++ java/first.java
@@ -0,0 +1,28 @@
+public class first {
+    public static void main(String[] args) {
+
+        // printing hello wolrd;
+
+        System.out.println("Hello World");
+
+        // Declaring variable
+
+        int myNum = 5;
+        System.out.println(myNum);
+
+        double Mydouble = myNum;
+        System.out.println(Mydouble);
+
+        // adding the variables
+
+        int num1 = 10;
+        int num2 = 20;
+        int total = num1 + num2;
+        System.out.println(total);
+
+        // counting the text
+        String text = "HAIDENVENKATDEEPAKKAUSHIKPRASANNAASWATHVIMAL";
+        System.out.println("The total number of letters:" + text.length());
+
+    }
+}

a8afe54a1dbec0a5c54ac81c5d6a2960218d2f0b
Haiden Arulappan <you@example.com>
2022-03-30 15:17:34 +0530

    add

--- java/operators.java
+++ java/operators.java
@@ -1,3 +1,31 @@
 public class operators {
+    public static void main(String[] args) {
 
+        // Arthimetic operators(+,-,*,/,%,++,--)
+
+        int add = 100 + 50;
+        System.out.println(add);
+
+        int num1 = 100;
+        int num2 = 900;
+        int total = num2 - num1;
+        System.out.println(total);
+
+        // Assignment operators(=.+=,-=,*=,/=,%=,&=,|=,^=,>>=,<<=)
+
+        int num3 = 90;
+        num3 += 25;
+        System.out.println(num3);
+
+        // Comparison operators(==,!=,>,<,>=,<=)
+
+        int num4 = 10;
+        int num5 = 10;
+        System.out.println(num4 == num5);
+
+        // Logical operators(&&,||,!)
+
+        int num6 = 10;
+        System.out.println(num6 > 4 && num6 < 15);
+    }
 }
